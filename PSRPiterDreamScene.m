b3bfdccdc818072fd044c400c7d14d06e7b11bd5
Nikolay Shubenkov <n.shubenkov@gmail.com>
2014-10-17 21:40:45 +0400

    added player

--- PSRPiterDreamScene.m
+++ PSRPiterDreamScene.m
@@ -8,13 +8,19 @@
 
 #import "PSRPiterDreamScene.h"
 
+#import "CGPointExtension.h"
+
 @interface PSRPiterDreamScene() {
 
+    CCScene *_player;
+    
     CCSprite *_topBackground1;
     CCSprite *_topBackground2;
     
     CCSprite *_bottomBackground1;
     CCSprite *_bottomBackground2;
+    
+    CCPhysicsNode *_physicalWorld;
 }
 
 @end
@@ -31,11 +37,38 @@
     self.userInteractionEnabled = YES;
 //    [self setupLabels];
     [self setupBackgrounds];
-//    [self setupPhysics];
+    [self setupPhysics];
 //    [self setupButtons];
-//    [self setupPlayer];
+    [self setupPlayer];
     // done
     return self;
+}
+
+- (void)setupPlayer
+{
+    _player = [CCSprite spriteWithImageNamed:@"PiterIcon.png"];
+    _player.position    = ccp(self.contentSize.width / 2 , self.contentSize.height / 2);
+    _player.anchorPoint = ccp(0.5,0.5);
+    
+    _player.physicsBody.mass = 100;
+    _player.physicsBody.affectedByGravity = YES;
+    _player.physicsBody.type = CCPhysicsBodyTypeStatic;
+    _player.physicsBody      = [CCPhysicsBody bodyWithRect:(CGRect){ CGPointZero,_player.contentSize }
+                                                            cornerRadius:15];
+    //    _player.physicsBody.sensor         = YES;
+//    _player.physicsBody.collisionGroup = kPSRPlayerCollisionGroupName;
+//    _player.physicsBody.collisionType  = kPSRPlayerCollisionType;
+//    
+    [_physicalWorld addChild: _player];
+}
+
+- (void)setupPhysics
+{
+    _physicalWorld = [CCPhysicsNode node];
+    _physicalWorld.gravity   = ccp(0, -350);
+    //    _physicalWorld.debugDraw = YES;
+//    _physicalWorld.collisionDelegate = self;
+    [self addChild:_physicalWorld];
 }
 
 #pragma mark - Update
@@ -44,9 +77,28 @@
 {
     static const CGFloat piterSpeed = 100;
     
+    [self updatePlayerRotation];
+    
     [self scrollButtomBackgroundsOnDelta:delta * piterSpeed];
     [self scrollTopBackgroundsOnDelta:delta    * piterSpeed / 3];
 }
+
+- (void)updatePlayerRotation
+{
+    CGFloat rotateAngleKoeff =  _player.physicsBody.velocity.y / 3;
+    if (rotateAngleKoeff < 0){
+        rotateAngleKoeff = MAX(-90, rotateAngleKoeff);
+    }
+    else {
+        rotateAngleKoeff = MIN(90, rotateAngleKoeff);
+    }
+    
+    CCActionRotateBy *rotateAction = [[CCActionRotateBy alloc]initWithDuration:0.2
+                                                                         angle:-rotateAngleKoeff / 2];
+    [_player stopAllActions];
+    [_player runAction:rotateAction];
+}
+
 
 #pragma mark - Scroll Backgrounds
 
@@ -109,6 +161,14 @@
     [self addChild:_bottomBackground2];
 }
 
+-(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    // -----------------------------------------------------------------------
+#pragma mark - Touch Handler
+    // -----------------------------------------------------------------------
+    
+    [_player.physicsBody applyForce:ccp(0,25000)];
+}
 
 
 
